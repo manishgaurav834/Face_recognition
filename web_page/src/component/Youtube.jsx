fd50cd4a5ff0a379ea487fbe5de3b555eef63c66
a19hu <a19hu20osh1jkl@gmail.com>
2024-04-21 17:12:51 +0530

    final web page change

--- web_page/src/component/Youtube.jsx
+++ web_page/src/component/Youtube.jsx
@@ -4,8 +4,7 @@ function Youtube() {
   return (
     <div className="flex flex-col items-center justify-center">
       <p className='header_Problem'>Short Talk</p>
-
-      <iframe  className='youtube_video'  src="https://www.youtube.com/embed/8PTOkwze0Vw?si=iwI9et2P1e6LlMTb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/cpkRc22-674?si=mULIDMfU_RG6vhHw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
 }
